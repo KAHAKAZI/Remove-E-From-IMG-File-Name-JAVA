67a5b2280de12e95d7360d6978340808cd5057aa
kahakazi <kahakazi@gmail.com>
2020-11-29 22:12:42 +0100

    Adds source code, jar, README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Remove-E-From-IMG-Filename
+
+Execution:
+>java -jar RemovePatternFromFilename-2.0.jar
+
--- src/main/java/remove/Main.java
+++ src/main/java/remove/Main.java
@@ -7,14 +7,10 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) {
-        Path currentPath = Paths.get("");
-        if(args.length > 1){
-            System.out.println("Invalid number of arguments: " + args.length + ". Expected 1.");
+
+        Path currentPath = getPath(args);
+        if(currentPath == null)
             return;
-        }
-        if(args.length == 1){
-            currentPath = Paths.get(args[0].toString());
-        }
 
         System.out.println("Replace all E letters with empty sign in current directory for current files.");
         System.out.println("Ex. IMG_E123.JPG to IMG_123.JPG");
@@ -22,30 +18,49 @@ public class Main {
         String path = currentPath.toAbsolutePath().toString();
 
         System.out.println("Current path:\n" + path);
+
         File directory = new File(path);
         File[] files = directory.listFiles();
-        System.out.println("No of files: " + files.length);
 
-        int counter = 0;
+        if(files != null) {
+            System.out.println("No of files: " + files.length);
 
-        for (File file : files) {
-            String name = file.getName();
-            if(name.contains("IMG") && name.contains("E")){
-                counter++;
-                System.out.println("File: ");
-                System.out.println(name);
-                String newName = name.replace("E", "");
-                System.out.println("New: ");
-                System.out.println(newName);
-                File newFile = new File(newName);
-                file.renameTo(newFile);
+            int counter = 0;
+
+            for (File file : files) {
+                String name = file.getName();
+                if(name.contains("IMG") && name.contains("E")){
+                    counter++;
+                    System.out.println("File: ");
+                    System.out.println(name);
+                    String newName = name.replace("E", "");
+                    System.out.println("New: ");
+                    System.out.println(newName);
+                    File newFile = new File(newName);
+                    file.renameTo(newFile);
+                }
             }
-        }
-        if(counter == 0) {
-            System.out.println("Nothing to change");
+            if(counter == 0) {
+                System.out.println("Nothing to change");
+            } else {
+                System.out.println(counter + " files renamed.");
+            }
         } else {
-            System.out.println(counter + " files renamed.");
+            System.out.println("Invalid path specified.");
         }
-        // ctrl + shift + f10
+
+    }
+
+    private static Path getPath(String[] args){
+        // default "here" path
+        Path currentPath = Paths.get("");
+        if(args.length > 1){
+            System.out.println("Invalid number of arguments: " + args.length + ". Expected 1.");
+            return null;
+        }
+        if(args.length == 1){
+            currentPath = Paths.get(args[0].toString());
+        }
+        return currentPath;
     }
 }
